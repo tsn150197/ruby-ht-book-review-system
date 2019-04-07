7c58e2fd5a87402a40dc4635b05875874fbea9d3
tsn150197 <famaxlevel1997@gmail.com>
2019-04-07 16:44:27 +0700

    User view list all favotite books

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
       resources :markers, only: %i(create destroy)
     end
     resources :purchase_requests, only: %i(index destroy)
+    resources :favorites, only: %i(index destroy)
   end
 end
